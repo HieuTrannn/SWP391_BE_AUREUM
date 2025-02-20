564251070746c9f5b911f80c68fd602fe286c6be
HieuTrannn <hieutran4525@gmail.com>
2025-02-21 00:08:49 +0700

    feat/hieu: config cors

--- src/main/java/com/example/SkincareProductSales/SkincareProductSalesApplication.java
+++ src/main/java/com/example/SkincareProductSales/SkincareProductSalesApplication.java
@@ -1,9 +1,12 @@
 package com.example.SkincareProductSales;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "AUREUM-API", version = "2.0", description = "API"))
 public class SkincareProductSalesApplication {
 
 	public static void main(String[] args) {
