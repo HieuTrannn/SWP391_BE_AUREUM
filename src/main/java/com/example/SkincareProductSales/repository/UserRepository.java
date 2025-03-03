f7f71f5bd944f2a440fd8f4c0c1245dc7452807a
HieuTrannn <hieutran4525@gmail.com>
2025-03-03 17:30:01 +0700

    feat/hieu: add userRepository

---
+++ src/main/java/com/example/SkincareProductSales/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.SkincareProductSales.repository;
+
+import com.example.SkincareProductSales.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<Account, Long> {
+
+    Account findAccountById(long id);
+
+    List<Account> findAccountByIsActiveFalse();
+
+    List<Account> findAccountByIsActiveTrue();
+}
