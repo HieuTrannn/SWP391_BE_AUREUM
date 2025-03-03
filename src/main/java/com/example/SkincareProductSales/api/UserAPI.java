8c3bfc0714c450fb3dd98537ca214d07a2111076
HieuTrannn <hieutran4525@gmail.com>
2025-03-03 17:29:30 +0700

    feat/hieu: add userAPI

---
+++ src/main/java/com/example/SkincareProductSales/api/UserAPI.java
@@ -0,0 +1,49 @@
+package com.example.SkincareProductSales.api;
+
+import com.example.SkincareProductSales.entity.Account;
+import com.example.SkincareProductSales.entity.request.UserRequest;
+import com.example.SkincareProductSales.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin("*")
+public class UserAPI {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public ResponseEntity getAllUsers() {
+        List<Account> accounts = userService.getAllUsers();
+        return ResponseEntity.ok(accounts);
+    }
+
+    @GetMapping("/getAllUsersIsActiveFalse")
+    public ResponseEntity getAllUsersIsActiveFalse() {
+        List<Account> accounts = userService.getAllUsersIsActiveFalse();
+        return ResponseEntity.ok(accounts);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity getUserById(@PathVariable long id) {
+        Account account = userService.getAccountById(id);
+        return ResponseEntity.ok(account);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity updateUser(@PathVariable long id, @RequestBody UserRequest userRequest) {
+        Account updateAccount = userService.updateUser(id, userRequest);
+        return ResponseEntity.ok(updateAccount);
+    }
+
+    @PutMapping("{id}/isActive")
+    public ResponseEntity toggleUserIsActive(@PathVariable long id) {
+        Account updateAccount = userService.toggleUserIsActive(id);
+        return ResponseEntity.ok(updateAccount);
+    }
+}
