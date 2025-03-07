833839e60c7dd3138b4089fd548d9eea053e2285
phuchau23 <phuchaulua123@gmail.com>
2025-03-07 12:05:37 +0700

    feat/Hau: Create AuthorizeException

---
+++ src/main/java/com/example/SkincareProductSales/exception/exceptions/AuthorizeException.java
@@ -0,0 +1,8 @@
+package com.example.SkincareProductSales.exception.exceptions;
+
+public class AuthorizeException extends RuntimeException{
+
+    public AuthorizeException(String message){
+        super(message);
+    }
+}
