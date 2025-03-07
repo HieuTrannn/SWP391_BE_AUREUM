24583c71bddd0b2887c565473b0002d3bf594c4d
phuchau23 <phuchaulua123@gmail.com>
2025-03-07 12:09:52 +0700

    feat/Hau: create AccountUtils

---
+++ src/main/java/com/example/SkincareProductSales/utils/AccountUtils.java
@@ -0,0 +1,28 @@
+package com.example.SkincareProductSales.utils;
+
+import com.example.SkincareProductSales.entity.Account;
+import com.example.SkincareProductSales.repository.AuthenticationRepository;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountUtils implements ApplicationContextAware {
+
+    private static AuthenticationRepository userRepo;
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        userRepo = applicationContext.getBean(AuthenticationRepository.class);
+    }
+
+    public Account getCurrentAccount() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepo.findByUsername(username).orElseThrow();
+    }
+
+}
