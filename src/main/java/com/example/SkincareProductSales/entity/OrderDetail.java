cb6c9b58abde0e759d383a42073af6a44c1a7f38
phuchau23 <phuchaulua123@gmail.com>
2025-03-07 11:49:41 +0700

    feat/Hau: Getter Setter OrderDetails

--- src/main/java/com/example/SkincareProductSales/entity/OrderDetail.java
+++ src/main/java/com/example/SkincareProductSales/entity/OrderDetail.java
@@ -18,10 +18,61 @@ public class OrderDetail {
 
     @ManyToOne
     @JoinColumn(name = "order_id")
-            @JsonIgnore
+    @JsonIgnore
     Order order;
 
     @ManyToOne
     @JoinColumn(name = "product_id")
     Product product;
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(long id, int quantity, float price, Order order, Product product) {
+        this.id = id;
+        this.quantity = quantity;
+        this.price = price;
+        this.order = order;
+        this.product = product;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
