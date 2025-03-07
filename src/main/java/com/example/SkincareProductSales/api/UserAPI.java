199a63e3cadcbce337917b31a4546154370f5262
phuchau23 <phuchaulua123@gmail.com>
2025-03-07 12:11:40 +0700

    feat/Hau: config authen

--- src/main/java/com/example/SkincareProductSales/api/UserAPI.java
+++ src/main/java/com/example/SkincareProductSales/api/UserAPI.java
@@ -3,6 +3,7 @@ package com.example.SkincareProductSales.api;
 import com.example.SkincareProductSales.entity.Account;
 import com.example.SkincareProductSales.entity.request.UserRequest;
 import com.example.SkincareProductSales.service.UserService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin("*")
+@SecurityRequirement(name = "api")
+
 public class UserAPI {
 
     @Autowired
