ad42e1a7e71140fb50f7c4ca5e869e36423e0c69
phuchau23 <phuchaulua123@gmail.com>
2025-03-07 12:08:35 +0700

    feat/Hau: Add createUrlPayment, getAll, getOrdersByUser, update Status, fix create

--- src/main/java/com/example/SkincareProductSales/service/OrderService.java
+++ src/main/java/com/example/SkincareProductSales/service/OrderService.java
@@ -6,15 +6,28 @@ import com.example.SkincareProductSales.entity.OrderDetail;
 import com.example.SkincareProductSales.entity.Product;
 import com.example.SkincareProductSales.entity.request.OrderDetailRequest;
 import com.example.SkincareProductSales.entity.request.OrderRequest;
+import com.example.SkincareProductSales.entity.response.OrderResponse;
+import com.example.SkincareProductSales.enums.OrderStatusEnum;
 import com.example.SkincareProductSales.repository.AuthenticationRepository;
 import com.example.SkincareProductSales.repository.OrderRepository;
 import com.example.SkincareProductSales.repository.ProductRepository;
+
+import com.example.SkincareProductSales.utils.AccountUtils;
+import org.aspectj.weaver.ast.Or;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
@@ -28,23 +41,123 @@ public class OrderService {
     @Autowired
     ProductRepository productRepository;
 
-    public Order create(OrderRequest orderRequest){
+    @Autowired
+    AccountUtils accountUtils;
+
+
+
+    public Order updateStatus(OrderStatusEnum status, long id){
+        Order order = orderRepository.findOrderById(id);
+        order.setStatus(status);
+        return orderRepository.save(order);
+    }
+
+    public String create(OrderRequest orderRequest) throws Exception{
+
+        float total = 0;
 
         List<OrderDetail> orderDetails = new ArrayList<>();
         Order order = modelMapper.map(orderRequest, Order.class);
+        order.setAccount(accountUtils.getCurrentAccount());
 
         for (OrderDetailRequest orderDetailRequest: orderRequest.getDetails()){
             OrderDetail orderDetail = new OrderDetail();
             Product product = productRepository.findProductById(orderDetailRequest.getProductId());
-            orderDetail.setProduct(product);
-            orderDetail.setQuantity(orderDetailRequest.getQuantity());
-            orderDetail.setPrice(product.getPrice());
-            orderDetail.setOrder(order);
-            orderDetails.add(orderDetail);
+
+             if (product.getQuantity() >= orderDetailRequest.getQuantity()){
+                orderDetail.setProduct(product);
+                orderDetail.setQuantity(orderDetailRequest.getQuantity());
+                orderDetail.setPrice(product.getPrice() * orderDetailRequest.getProductId());
+                orderDetail.setOrder(order);
+                orderDetails.add(orderDetail);
+                product.setQuantity(product.getQuantity() - orderDetailRequest.getQuantity());
+                productRepository.save(product);
+                total += orderDetail.getPrice();
+            }else {
+                throw new RuntimeException("Product was sold out!!");
+            }
+
         }
 
+        order.setTotal(total);
         order.setOrderDetails(orderDetails);
 //        order.setOrderDetails(orderDetails);
-        return orderRepository.save(order);
+        Order newOrder = orderRepository.save(order);
+        return createUrlPayment(newOrder);
+    }
+
+    public List<Order> getOrdersByUser(){
+        Account account = accountUtils.getCurrentAccount();
+        return orderRepository.findAllByAccountId(account.getId());
+    }
+
+    public List<Order> getALl(){
+        return orderRepository.findAll();
+    }
+    public String createUrlPayment(Order order) throws Exception{
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        LocalDateTime createDate = LocalDateTime.now();
+        String formattedCreateDate = createDate.format(formatter);
+        String orderId = UUID.randomUUID().toString().substring(0, 6);
+
+        String tmnCode = "9DZT14DS";
+        String secretKey = "ND04E7FEO088030GBYXWTV917RTEDN33";
+        String vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+        String returnUrl = "http://localhost:8081/?orderId=" +order.getId();
+
+        String currCode = "VND";
+        Map<String, String> vnpParams = new TreeMap<>();
+        vnpParams.put("vnp_Version", "2.1.0");
+        vnpParams.put("vnp_Command", "pay");
+        vnpParams.put("vnp_TmnCode", tmnCode);
+        vnpParams.put("vnp_Locale", "vn");
+        vnpParams.put("vnp_CurrCode", currCode);
+        vnpParams.put("vnp_TxnRef", orderId);
+        vnpParams.put("vnp_OrderInfo", "Thanh toan cho ma GD: " + orderId);
+        vnpParams.put("vnp_OrderType", "other");
+        vnpParams.put("vnp_Amount",(int) order.getTotal() + "00");
+        vnpParams.put("vnp_ReturnUrl", returnUrl);
+        vnpParams.put("vnp_CreateDate", formattedCreateDate);
+        vnpParams.put("vnp_IpAddr", "167.99.74.201");
+        System.out.println("order.getTotal(): " + order.getTotal());
+        System.out.println("vnp_Amount: " + (int) order.getTotal() * 100);
+
+        StringBuilder signDataBuilder = new StringBuilder();
+        for (Map.Entry<String, String> entry : vnpParams.entrySet()) {
+            signDataBuilder.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8.toString()));
+            signDataBuilder.append("=");
+            signDataBuilder.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8.toString()));
+            signDataBuilder.append("&");
+        }
+        signDataBuilder.deleteCharAt(signDataBuilder.length() - 1); // Remove last '&'
+
+        String signData = signDataBuilder.toString();
+        String signed = generateHMAC(secretKey, signData);
+
+        vnpParams.put("vnp_SecureHash", signed);
+
+        StringBuilder urlBuilder = new StringBuilder(vnpUrl);
+        urlBuilder.append("?");
+        for (Map.Entry<String, String> entry : vnpParams.entrySet()) {
+            urlBuilder.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8.toString()));
+            urlBuilder.append("=");
+            urlBuilder.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8.toString()));
+            urlBuilder.append("&");
+        }
+        urlBuilder.deleteCharAt(urlBuilder.length() - 1); // Remove last '&'
+        return urlBuilder.toString();
+    }
+
+    private String generateHMAC(String secretKey, String signData) throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac hmacSHh512 = Mac.getInstance("HmacShA512");
+        SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "hmacShA512");
+        hmacSHh512.init(keySpec);
+        byte[] hmacBytes = hmacSHh512.doFinal(signData.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder result = new StringBuilder();
+        for (byte b : hmacBytes) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
     }
 }
