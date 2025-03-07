b546a1019d729e5358361cdaac2336a379bf8631
phuchau23 <phuchaulua123@gmail.com>
2025-03-07 11:59:36 +0700

    feat/Hau: add EnumStatus and getter setter order

--- src/main/java/com/example/SkincareProductSales/entity/Order.java
+++ src/main/java/com/example/SkincareProductSales/entity/Order.java
@@ -1,14 +1,18 @@
 package com.example.SkincareProductSales.entity;
 
+import com.example.SkincareProductSales.enums.OrderStatusEnum;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "orders")
 public class Order {
@@ -17,16 +21,75 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public long id;
 
+
     public Date createAt;
     public float total;
+    public OrderStatusEnum status = OrderStatusEnum.IN_PROCESS;
 
     @ManyToOne
     @JoinColumn(name = "account_id")
     Account account;
 
-
-
-
     @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
     List<OrderDetail> orderDetails = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+
+    public OrderStatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatusEnum status) {
+        this.status = status;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public List<OrderDetail> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetail> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+
+    public Order() {
+    }
+
+    public Order(long id, Date createAt, float total, OrderStatusEnum status, Account account, List<OrderDetail> orderDetails) {
+        this.id = id;
+        this.createAt = createAt;
+        this.total = total;
+        this.status = status;
+        this.account = account;
+        this.orderDetails = orderDetails;
+    }
 }
