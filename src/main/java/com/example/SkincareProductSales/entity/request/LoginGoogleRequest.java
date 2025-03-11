0d3fc8190eb86ded6347b9bd28bdcd4c09b69a96
HieuTrannn <hieutran4525@gmail.com>
2025-03-11 17:27:40 +0700

    feat/Hieu: add LoginGoogleRequest

---
+++ src/main/java/com/example/SkincareProductSales/entity/request/LoginGoogleRequest.java
@@ -0,0 +1,12 @@
+package com.example.SkincareProductSales.entity.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginGoogleRequest {
+    private String token;
+}
