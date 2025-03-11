6a0072b758b596e983926a306ac7cead5cb072cf
HieuTrannn <hieutran4525@gmail.com>
2025-03-12 03:00:41 +0700

    feat/hieu: add Skin Request and Response

---
+++ src/main/java/com/example/SkincareProductSales/entity/response/SkinResponse.java
@@ -0,0 +1,21 @@
+package com.example.SkincareProductSales.entity.response;
+
+public class SkinResponse {
+
+    public String name;
+
+    public SkinResponse() {
+    }
+
+    public SkinResponse(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
