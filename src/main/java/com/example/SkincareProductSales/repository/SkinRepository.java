63ddce0d02aec97fbd1529df3767124d41818b63
HieuTrannn <hieutran4525@gmail.com>
2025-03-12 03:01:16 +0700

    feat/hieu: add findSkinByIsDeleted

--- src/main/java/com/example/SkincareProductSales/repository/SkinRepository.java
+++ src/main/java/com/example/SkincareProductSales/repository/SkinRepository.java
@@ -3,7 +3,13 @@ package com.example.SkincareProductSales.repository;
 import com.example.SkincareProductSales.entity.Skin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SkinRepository extends JpaRepository<Skin,Long> {
 
     Skin findSkinById(long id);
+
+    List<Skin> findSkinsByIsDeletedFalse();
+
+    List<Skin> findSkinsByIsDeletedTrue();
 }
