64ecc8cb865d6d8962a9f5ff5245750ae036b1d3
HieuTrannn <hieutran4525@gmail.com>
2025-03-11 17:31:01 +0700

    fix/hieu: delete import no use

--- src/main/java/com/example/SkincareProductSales/config/FirebaseConfig.java
+++ src/main/java/com/example/SkincareProductSales/config/FirebaseConfig.java
@@ -8,13 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
