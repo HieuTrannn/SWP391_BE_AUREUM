229829e4a9e84021c64521d3ea51f17e31739c2b
phuchau23 <phuchaulua123@gmail.com>
2025-03-13 15:20:49 +0700

    feat/Hau: create findOrder

--- src/main/java/com/example/SkincareProductSales/repository/OrderRepository.java
+++ src/main/java/com/example/SkincareProductSales/repository/OrderRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
     List<Order> findAllByAccountId(Long accountId);
+
+    Order findOrderById(Long id);
 }
