27e608a8b4086fc8e2216a71d0cb899d4ba0f4f0
HieuTrannn <hieutran4525@gmail.com>
2025-03-16 02:57:31 +0700

    fix/hieu: rename and fix createFeedback

---
+++ src/main/java/com/example/SkincareProductSales/api/RatingAPI.java
@@ -0,0 +1,46 @@
+package com.example.SkincareProductSales.api;
+
+import com.example.SkincareProductSales.entity.Rating;
+import com.example.SkincareProductSales.entity.request.RatingRequest;
+import com.example.SkincareProductSales.service.RatingService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rating")
+@SecurityRequirement(name = "api")
+@CrossOrigin("*")
+public class RatingAPI {
+
+    @Autowired
+    RatingService ratingService;
+
+    @PostMapping
+    public ResponseEntity createRating (@Valid @RequestBody RatingRequest ratingRequest) {
+        Rating rating = ratingService.createRating(ratingRequest);
+        return ResponseEntity.ok(rating);
+    }
+
+    @GetMapping
+    public ResponseEntity getAllRating () {
+        List<Rating> ratings = ratingService.getAllRating();
+        return ResponseEntity.ok(ratings);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity getRatingById(@PathVariable long id) {
+        Rating rating = ratingService.getRatingById(id);
+        return ResponseEntity.ok(rating);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity updateRating (@PathVariable long id, @Valid @RequestBody RatingRequest ratingRequest) {
+        Rating updateRating = ratingService.updateRating(id, ratingRequest);
+        return ResponseEntity.ok(updateRating);
+    }
+}
