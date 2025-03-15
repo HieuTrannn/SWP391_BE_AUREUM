ebd0817e3e3c42c89dee5e8cc38245cb35feac3d
HieuTrannn <hieutran4525@gmail.com>
2025-03-15 23:39:22 +0700

    feat/hieu: add VoucherRepository

---
+++ src/main/java/com/example/SkincareProductSales/repository/VoucherRepository.java
@@ -0,0 +1,17 @@
+package com.example.SkincareProductSales.repository;
+
+import com.example.SkincareProductSales.entity.Voucher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface VoucherRepository extends JpaRepository<Voucher, Long> {
+    Voucher findVoucherById(long id);
+
+    Optional<Voucher> findVoucherByCode(String code);
+
+    List<Voucher> findVouchersByIsDeletedFalse();
+
+    List<Voucher> findVouchersByIsDeletedTrue();
+}
